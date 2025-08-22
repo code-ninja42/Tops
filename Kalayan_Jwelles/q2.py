42a9b2d9493ba8afcbd7050607eeb708113bf0a1
code-ninja42 <143476719+code-ninja42@users.noreply.github.com>
2025-08-22 22:36:45 +0530

    Add files via upload

---
+++ Kalayan_Jwelles/q2.py
@@ -0,0 +1,18 @@
+num1 = int(input())
+num2 = int(input())
+if num1 > 0:
+    print(f"{num1} is positive")
+else:
+    print(f"{num1} is negative ")
+if num2 > 0:
+    print(f"{num2} is postive")
+else:
+    print(f"{num2} is negative")
+if num1 % 2==0:
+    print(f"{num1} is even")
+else:
+    print(f"{num1} is odd ")
+if num2 % 2==0:
+    print(f"{num2} is even")
+else:
+    print(f"{num2} is odd")
