5fc113caced5f7feb6a41c6ad948d6f36b2ad08a
code-ninja42 <143476719+code-ninja42@users.noreply.github.com>
2025-08-30 18:27:47 +0530

    Delete que12.py

--- que12.py
+++
@@ -1,6 +0,0 @@
-"""
-12.Write a program in C to find the number of times a given word 'is' appears in
-the given string.
-"""
-string = "is something happens ? is somethinf weird? is it "
-print("is apperes:",string.count("is"))
